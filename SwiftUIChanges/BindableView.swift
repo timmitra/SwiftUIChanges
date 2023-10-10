1d3524b99dfccd368fe665dd3afc86ce4ca618d5
Tim Mitra <tim@it-guy.com>
2023-10-10 02:45:50 -0400

    using Observable from SwiftData, we can send the bindable status of the toggle to the parent view and change the background color

---
+++ SwiftUIChanges/BindableView.swift
@@ -0,0 +1,18 @@
+//
+//  Bindable.swift
+//  SwiftUIChanges
+//
+//  Created by Tim Mitra on 2023-10-10.
+//
+
+import SwiftUI
+
+struct BindableView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BindableView()
+}
