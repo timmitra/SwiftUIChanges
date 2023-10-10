b4ebd218065a7a53b1d6c83fceb73c4f8fb49807
Tim Mitra <tim@it-guy.com>
2023-10-10 02:17:59 -0400

    Initial Commit

---
+++ SwiftUIChanges/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIChanges
+//
+//  Created by Tim Mitra on 2023-10-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIChanges/SwiftUIChangesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChangesApp.swift
+//  SwiftUIChanges
+//
+//  Created by Tim Mitra on 2023-10-10.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIChangesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
